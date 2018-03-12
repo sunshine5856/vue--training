9f3dbbe24a6dca70e84d5a1b70b1d31b77646f25
huangqq <1872367346@q.com>
2018-03-12 08:49:56 +0800

    there are some changes

--- src/routes.js
+++ src/routes.js
@@ -7,9 +7,7 @@
 
 import Login from './views/Login.vue'
 import NotFound from './views/404.vue'
-import Home from './views/Home.vue'
 import Layout from './views/Layout.vue'
-import Nav from './views/Nav.vue'
 import Dashboard from './views/Dashboard.vue'
 
 
@@ -19,71 +17,9 @@ import Form from './views/menu/Form.vue'
 import user from './views/menu/user.vue'
 import Page4 from './views/nav2/Page4.vue'
 import Page5 from './views/nav2/Page5.vue'
-import Page6 from './views/nav3/Page6.vue'
-import echarts from './views/charts/echarts.vue'
+import echarts from './views/draft/echarts.vue'
+// import Test from './views/draft/Test.vue'
 
-
-//登录页
-// export const loginRouter = {
-//     path: '/login',
-//     component: Login,
-//     name: '',
-//     hidden: true
-// };
-
-// const routes = new VueRouter({
-//     routes: [
-//         //login
-//         {
-//             path: '/login',
-//             component: Login,
-//             name: '',
-//             hidden: true
-//         },
-//         //404
-//         {
-//             path: '/404',
-//             component: NotFound,
-//             name: '',
-//             hidden: true
-//         },
-//         //menu
-//         {
-//             path: '/',
-//             component: Dashboard,
-//             // redirect: '/dashboard',
-//             hidden: true,
-//             children: [
-//                 {
-//                     path: 'dashboard',
-//                     component: Home,
-//                     name: '首页',
-//                     // meta: {title: '首页',noCache: true}
-//                 }]
-//         },
-//         // {
-//         //     path: '/user/:id', component: User,
-//         //     children: [
-//         //         {
-//         //             // 当 /user/:id/profile 匹配成功，
-//         //             // UserProfile 会被渲染在 User 的 <router-view> 中
-//         //             path: 'profile',
-//         //             component: UserProfile
-//         //         },
-//         //         {
-//         //             // 当 /user/:id/posts 匹配成功
-//         //             // UserPosts 会被渲染在 User 的 <router-view> 中
-//         //             path: 'posts',
-//         //             component: UserPosts
-//         //         }
-//         //     ]
-//         // }
-//     ]
-// })
-//作为子路由但是不显示在左侧的侧边栏
-
-
-//作为子路由写在左侧的侧边栏
 
 let routes = [
     //login
@@ -118,7 +54,7 @@ let routes = [
         iconCls: 'fa fa-address-card',
         leaf: true,//只有一个节点
         children: [
-              {path: '/table', component: Table, name: '目录清单'},
+            {path: '/test', component: Table, name: '目录清单'},
         ]
     },
     {
@@ -139,8 +75,8 @@ let routes = [
         name: '审核库',
         iconCls: 'fa fa-id-card-o',
         children: [
-            {path: '/menu/page4', component: Page4, name: '页面4'},
-            {path: '/menu/page5', component: Page5, name: '页面5'}
+            {path: '/menu/page4', component: Page4, name: '审核库一'},
+            {path: '/menu/page5', component: Page5, name: '审核库二'}
         ]
     },
 
@@ -150,37 +86,18 @@ let routes = [
         name: '待发布',
         iconCls: 'fa fa-bar-chart',
         children: [
-            {path: '/menu/echarts', component: echarts, name: 'echarts'}
+            {path: '/menu/echarts', component: echarts, name: '待发布一'}
         ]
     },
-    {
-        path: '/menu',
-        component: Layout,
-        name: '发布库',
-        iconCls: 'fa fa-bar-chart',
-        children: [
-            {path: '/menu/echarts', component: echarts, name: 'echarts'}
-        ]
-    },
-    {
-        path: '/menu',
-        component: Layout,
-        name: '目录导入',
-        iconCls: 'fa fa-bar-chart',
-        children: [
-            {path: '/menu/echarts', component: echarts, name: 'echarts'}
-        ]
-    },
-
 
     //实施清单
     {
         path: '/implement',
         component: Layout,
-        name: '',
+        name: '实施清单',
         iconCls: 'fa fa-bar-chart',
         children: [
-            {path: '/menu/echarts', component: echarts, name: 'echarts'}
+            {path: '/implement/echarts', component: echarts, name: 'echarts'}
         ]
     },
 
@@ -191,7 +108,27 @@ let routes = [
         name: '目录导入',
         iconCls: 'fa fa-bar-chart',
         children: [
-            {path: '/menu/echarts', component: echarts, name: 'echarts'}
+            {path: '/duty/menu', component: echarts, name: '目录导入一'}
+        ]
+    },
+    //流程管理
+    {
+        path: '/progress',
+        component: Layout,
+        name: '流程管理',
+        iconCls: 'fa fa-bar-chart',
+        children: [
+            {path: '/progress/menu', component: echarts, name: '流程管理一'}
+        ]
+    },
+    //统计分析
+    {
+        path: '/count',
+        component: Layout,
+        name: '发布库',
+        iconCls: 'fa fa-bar-chart',
+        children: [
+            {path: '/count/publish', component: echarts, name: '发布库一'}
         ]
     },
     {
