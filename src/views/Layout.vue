bab442c72b140cfa6896ac9cf0e69d4ba9a0523e
huangqq <1872367346@q.com>
2018-04-20 18:04:24 +0800

    there are some changes

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,3 +1,9 @@
+<style lang="scss">
+    @import '../styles/dashboard.scss';
+    @import '../styles/vars.scss';
+    @import '../styles/common.scss';
+
+</style>
 <template>
     <el-row class="container">
         <el-col :span="24" class="header">
@@ -29,12 +35,12 @@
                 </router-link>
             </el-col>
             <el-col :span="2">
-                <router-link class="inlineBlock nav-link nav-five" to="/menu">
+                <router-link class="inlineBlock nav-link nav-five" to="/progress">
                     <span class="nav-top">流程管理</span>
                 </router-link>
             </el-col>
             <el-col :span="2">
-                <router-link class="inlineBlock nav-link nav-six" to="/menu">
+                <router-link class="inlineBlock nav-link nav-six" to="/count">
                     <span class="nav-top">统计分析</span>
                 </router-link>
             </el-col>
@@ -52,74 +58,100 @@
         </el-col>
         <el-col :span="24" class="main">
             <aside :class="collapsed?'menu-collapsed':'menu-expanded'">
-                <!--导航菜单-->
-                <el-menu :default-active="$route.path" class="el-menu-vertical-demo" @open="handleopen"
+                <!--左侧导航菜单-->
+                <el-menu :default-active="$route.path" class="el-menu-vertical-demo menu-box" @open="handleopen"
+                         style="border-radius:0;"
                          @close="handleclose" @select="handleselect"
                          unique-opened router v-show="!collapsed">
-                    <template v-for="(item,index) in $router.options.routes" v-if="!item.hidden">
-                        <el-submenu :index="index+''" v-if="!item.leaf">
-                            <template slot="title"><i :class="item.iconCls"></i>{{item.name}}</template>
+                    <template v-for="(item,index) in $router.options.routes" v-if="!item.hidden"
+                              style="border-radius:0;">
+                        <!--多个节点-->
+                        <el-submenu :index="index+''" v-if="!item.leaf" class="parents-title" style="border-radius:0;">
+                            <template slot="title" class="child-title"><i :class="item.iconCls"></i>{{item.name}}
+                            </template>
                             <el-menu-item v-for="child in item.children" :index="child.path" :key="child.path"
-                                          v-if="!child.hidden">{{child.name}}
+                                          class="child-title"
+                                          v-if="!child.hidden" style="border-radius:0;">{{child.name}}
                             </el-menu-item>
                         </el-submenu>
-                        <el-menu-item v-if="item.leaf&&item.children.length>0" :index="item.children[0].path"><i
-                                :class="item.iconCls"></i>{{item.children[0].name}}
+                        <!--单个节点-->
+                        <el-menu-item v-if="item.leaf&&item.children.length>0" :index="item.children[0].path">
+                            <i :class="item.iconCls"></i>{{item.children[0].name}}
                         </el-menu-item>
                     </template>
                 </el-menu>
-                <!--导航菜单-折叠后-->
-                <ul class="el-menu el-menu-vertical-demo collapsed" v-show="collapsed" ref="menuCollapsed">
-                    <li v-for="(item,index) in $router.options.routes" v-if="!item.hidden" class="el-submenu item">
-                        <template v-if="!item.leaf">
-                            <div class="el-submenu__title" style="padding-left: 20px;" @mouseover="showMenu(index,true)"
-                                 @mouseout="showMenu(index,false)"><i :class="item.iconCls"></i></div>
-                            <ul class="el-menu submenu" :class="'submenu-hook-'+index" @mouseover="showMenu(index,true)"
-                                @mouseout="showMenu(index,false)">
-                                <li v-for="child in item.children" v-if="!child.hidden" :key="child.path"
-                                    class="el-menu-item" style="padding-left: 40px;"
-                                    :class="$route.path==child.path?'is-active':''" @click="$router.push(child.path)">
-                                    {{child.name}}
-                                </li>
-                            </ul>
-                        </template>
-                        <template v-else>
-                    <li class="el-submenu">
-                        <div class="el-submenu__title el-menu-item"
-                             style="padding-left: 20px;height: 56px;line-height: 56px;padding: 0 20px;"
-                             :class="$route.path==item.children[0].path?'is-active':''"
-                             @click="$router.push(item.children[0].path)"><i :class="item.iconCls"></i></div>
-                    </li>
-</template>
-</li>
-</ul>
-</aside>
-<section class="content-container">
-    <div class="grid-content bg-purple-light">
-        <el-col :span="24" class="breadcrumb-container">
-            <strong class="title">{{$route.name}}</strong>
-            <el-breadcrumb separator="/" class="breadcrumb-inner">
-                <el-breadcrumb-item v-for="item in $route.matched" :key="item.path">
-                    {{ item.name }}
-                </el-breadcrumb-item>
-            </el-breadcrumb>
+            </aside>
+            <!--<el-aside width="200px" style="background-color: rgb(238, 241, 246)">-->
+            <!--<el-menu :default-openeds="['1', '3']">-->
+            <!--<el-submenu index="1">-->
+            <!--<template slot="title">-->
+            <!--<i class="el-icon-message">-->
+
+            <!--</i>草稿库-->
+            <!--</template>-->
+            <!--<el-menu-item-group>-->
+            <!--<template slot="title">分组一</template>-->
+            <!--<el-menu-item index="1-1">选项1</el-menu-item>-->
+            <!--<el-menu-item index="1-2">选项2</el-menu-item>-->
+            <!--</el-menu-item-group>-->
+            <!--<el-menu-item-group title="分组2">-->
+            <!--<el-menu-item index="1-3">选项3</el-menu-item>-->
+            <!--</el-menu-item-group>-->
+            <!--<el-submenu index="1-4">-->
+            <!--<template slot="title">选项4</template>-->
+            <!--<el-menu-item index="1-4-1">选项4-1</el-menu-item>-->
+            <!--</el-submenu>-->
+            <!--</el-submenu>-->
+            <!--<el-submenu index="2">-->
+            <!--<template slot="title"><i class="el-icon-menu"></i>导航二</template>-->
+            <!--<el-menu-item-group>-->
+            <!--<template slot="title">分组一</template>-->
+            <!--<el-menu-item index="2-1">选项1</el-menu-item>-->
+            <!--<el-menu-item index="2-2">选项2</el-menu-item>-->
+            <!--</el-menu-item-group>-->
+            <!--<el-menu-item-group title="分组2">-->
+            <!--<el-menu-item index="2-3">选项3</el-menu-item>-->
+            <!--</el-menu-item-group>-->
+            <!--<el-submenu index="2-4">-->
+            <!--<template slot="title">选项4</template>-->
+            <!--<el-menu-item index="2-4-1">选项4-1</el-menu-item>-->
+            <!--</el-submenu>-->
+            <!--</el-submenu>-->
+            <!--&lt;!&ndash;<el-submenu index="3">&ndash;&gt;-->
+            <!--&lt;!&ndash;<template slot="title"><i class="el-icon-setting"></i>导航三</template>&ndash;&gt;-->
+            <!--&lt;!&ndash;<el-menu-item-group>&ndash;&gt;-->
+            <!--&lt;!&ndash;<template slot="title">分组一</template>&ndash;&gt;-->
+            <!--&lt;!&ndash;<el-menu-item index="3-1">选项1</el-menu-item>&ndash;&gt;-->
+            <!--&lt;!&ndash;<el-menu-item index="3-2">选项2</el-menu-item>&ndash;&gt;-->
+            <!--&lt;!&ndash;</el-menu-item-group>&ndash;&gt;-->
+            <!--&lt;!&ndash;<el-menu-item-group title="分组2">&ndash;&gt;-->
+            <!--&lt;!&ndash;<el-menu-item index="3-3">选项3</el-menu-item>&ndash;&gt;-->
+            <!--&lt;!&ndash;</el-menu-item-group>&ndash;&gt;-->
+            <!--&lt;!&ndash;<el-submenu index="3-4">&ndash;&gt;-->
+            <!--&lt;!&ndash;<template slot="title">选项4</template>&ndash;&gt;-->
+            <!--&lt;!&ndash;<el-menu-item index="3-4-1">选项4-1</el-menu-item>&ndash;&gt;-->
+            <!--&lt;!&ndash;</el-submenu>&ndash;&gt;-->
+            <!--&lt;!&ndash;</el-submenu>&ndash;&gt;-->
+            <!--</el-menu>-->
+            <!--</el-aside>-->
+            <section class="content-container">
+                <div class="grid-content bg-purple-light">
+                    <el-col :span="24" class="content-wrapper">
+                        <transition name="fade" mode="out-in">
+                            <router-view></router-view>
+                        </transition>
+                    </el-col>
+                </div>
+            </section>
         </el-col>
-        <el-col :span="24" class="content-wrapper">
-            <transition name="fade" mode="out-in">
-                <router-view></router-view>
-            </transition>
-        </el-col>
-    </div>
-</section>
-</el-col>
-</el-row>
+    </el-row>
 </template>
 
 <script>
     export default {
         data() {
             return {
-                sysName: 'VUEADMIN',
+                sysName: '',
                 collapsed: false,
                 sysUserName: '',
                 sysUserAvatar: '',
@@ -176,14 +208,9 @@
                 this.sysUserName = user.name || '';
                 this.sysUserAvatar = user.avatar || '';
             }
-
         }
     }
 
+
 </script>
 
-<style scoped lang="scss">
-    @import '../styles/nav.scss';
-    @import '../styles/vars.scss';
-
-</style>
